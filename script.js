433930cc1f346b22617c73991487cb739f95d9d0
Aminul islam <aminurrohman98@gmail.com>
2026-06-13 11:31:18 +0100

    practice about boolean

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+//this is sting variable
 let js = "amazing";
-// if (js === "amazing") alert("JavaScript is a FUN!");
-
-console.log(40 + 8 + 23 - 10);
-let PI = 3.1415;
 let myFirstJob = "Programmer";
 let country = "Bangladesh";
 let continent = "Asia";
 let population = "108 million people";
-console.log("this is about country:", country);
-console.log("this is about continent:", continent);
-console.log("this is about population:", population);
-let isIsland = false;
+
+//this is number variable
+let PI = 3.1415;
+
+//this is undefined variable
 let language;
-console.log(typeof isIsland);
-console.log(typeof language);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+
+// this is boolean  variable
+let isIsland = false;
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
