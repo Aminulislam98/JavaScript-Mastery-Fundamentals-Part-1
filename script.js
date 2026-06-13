595361cd04c1cd3fbb946a3252538dc0599c2af9
Aminul islam <aminurrohman98@gmail.com>
2026-06-13 11:12:52 +0100

    practice about variable types

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is a FUN!");
+// if (js === "amazing") alert("JavaScript is a FUN!");
 
 console.log(40 + 8 + 23 - 10);
 let PI = 3.1415;
 let myFirstJob = "Programmer";
+let country = "Bangladesh";
+let continent = "Asia";
+let population = "108 million people";
+console.log("this is about country:", country);
+console.log("this is about continent:", continent);
+console.log("this is about population:", population);
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
