04cad6a8f6115d6c7b81cf23c560cb20a38f2d41
Aminul islam <aminurrohman98@gmail.com>
2026-06-14 14:54:51 +0100

    exercised the challenge and passed the result :)

--- script.js
+++ script.js
@@ -131,3 +131,22 @@ if (money) {
 // }
 
 // Boolean Logic
+
+// Logical operator
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// Exercise challenge:
+// let scoreDolphins = (96 + 108 + 89) / 3;
+// let scoreKoalas = (88 + 91 + 110) / 3;
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log("Both win the trophy");
+// } else {
+//   console.log("Koalas win the trophy");
+// }
